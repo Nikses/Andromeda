fca85169dfc21be13a927ec612b6edaa47a21eff
Nikses <nvpelevin@gmail.com>
2024-05-01 23:26:01 +0100

    Updated logic for url and filename building

--- webscrape.py
+++ webscrape.py
@@ -33,11 +33,10 @@ def scrape_for_updates():
 
     return title, href
 
-url = 'https://www.govtrack.us/congress/bills/browse?status=passed#sort=-introduced_date&current_status[]=28'
 
+def scrape_bill_text_save_txt(href, title):
+    url = "https://www.govtrack.us" + href + "/text"
 
-def scrape_bill_text_save_txt():
-    url = "https://www.govtrack.us/congress/bills/118/hr7888/text"
     response = requests.get(url)
     if response.status_code != 200:
         print("Failed to retrieve the web page")
@@ -46,7 +45,9 @@ def scrape_bill_text_save_txt():
     soup = BeautifulSoup(response.text, 'html.parser')
     bill_text_container = soup.find_all('p')  # Assuming the bill text is in <p> tags
 
-    with open('test1.txt', 'w') as file:
+    log_title = title.split(' ')[1][:-1]
+    with open(f'scraped_logs/{log_title}.txt', 'w') as file:
+        file.write(title + '\n')  # Write the bill's title
         start_saving = False  # Flag to not save text unless it starts with "The text of the bill below"
         for paragraph in bill_text_container:
             text = paragraph.get_text()
@@ -57,5 +58,4 @@ def scrape_bill_text_save_txt():
             if start_saving:
                 file.write(text + '\n')
 
-
-scrape_bill_text_save_txt()
+    file.close()
